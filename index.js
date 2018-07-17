356558be6e7c1b450737b642512775ef2522affe
Fidel Thomet <me@fidelthomet.com>
2018-07-17 13:35:53 +0200

    [Update] convert country data to csv

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const { interpolateViridis } = require('d3-scale-chromatic')
 
 const args = process.argv.filter((d, i) => i > 2)
 const options = {
-  inspect: args.indexOf('-i') !== -1 || args.indexOf('-i') !== -1,
-  verbose: args.indexOf('-v') !== -1,
+  inspect: args.indexOf('-i') !== -1,
   path: process.argv[2],
-  preview: args.indexOf('-p') !== -1,
-  previewTime: args[args.indexOf('-p') + 1] || 0,
-  animation: args.indexOf('-a') !== -1,
   output: args.indexOf('-o') !== -1,
-  outputPath: args[args.indexOf('-o') + 1] || 'out.json'
+  outputPath: args[args.indexOf('-o') + 1] || `${process.argv[2]}.csv`
 }
 
 init()
@@ -28,9 +24,7 @@ function init () {
     return
   }
 
-  if (options.inspect || options.verbose) inspect(file)
-  if (options.preview) preview(file, options.previewTime)
-  if (options.animation) animate(file)
+  if (options.inspect) inspect(file)
   if (options.output) convert(file)
 }
 
@@ -39,100 +33,41 @@ function inspect (file) {
   const rootVariables = file.root.variables
   const rootDimensions = file.root.dimensions
 
+  console.log(file.root.variables.time.readSlice(0, 94))
+  console.log(Object.keys(file.root.variables.time.readSlice(0, 94)))
+
+
   // root
-  if (options.verbose) {
-    logH1('root')
-    logProps(file.root)
-  }
-  // attributes
-  logH1('attributes')
-  logAttributes(rootAttributes)
-  // dimensions
-  logH1('dimensions')
-  logDimensions(rootDimensions, true)
-  // variables
-  logH1('variables')
-  Object.keys(rootVariables).forEach(key => {
-    const {
-      name, type, attributes, dimensions
-    } = rootVariables[key]
-    // variable
-    if (options.verbose) {
-      logH2(name)
-      logProps(rootVariables[key])
-    } else {
-      logH2(`${name} (${type})`)
-    }
-    // dimensions
-    logH3('dimensions')
-    logDimensions(dimensions)
-    // attributes
-    logH3('attributes')
-    logAttributes(attributes)
-  })
-}
-
-function preview (file, time) {
-  const dimensions = file.root.dimensions
-  const variables = file.root.variables
-  const stride = Math.ceil(Math.max(
-    dimensions.lon.length / process.stdout.columns, dimensions.lat.length / (process.stdout.rows - 2)
-  ))
-  const width = Math.floor(dimensions.lon.length / stride)
-  const height = Math.floor(dimensions.lat.length / stride)
-
-  const key = Object.keys(variables).find(variable =>
-    Object.keys(dimensions).indexOf(variable) === -1
-  )
-
-  const missing = variables[key].attributes.missing_value.value
-
-  const data = variables[key].readStridedSlice(
-    time, 1, 1, // time
-    0, height, stride, // lat
-    0, width, stride // lon
-  )
-  const validData = data.filter(d => d !== missing && d > 0)
-  const max = Math.max(...validData)
-  const min = Math.min(...validData)
-
-  var scale = scaleLinear()
-    .domain([min, max])
-    .range([0, 1])
-
-  console.log(chalk.bold(`${variables[key].attributes.long_name.value} (${variables[key].attributes.units.value})`))
-  console.log(chalk.bold(`time: ${time}`))
-  process.stdout.write(`${min} `)
-  const legend = ' '.repeat(16 + 1).split('').map((d, i) => i / 16)
-  legend.forEach(d => {
-    process.stdout.write(chalk.bgHex(interpolateViridis(d))(` `))
-  })
-  process.stdout.write(` ${max}`)
-  process.stdout.write('\n')
-
-  data.forEach((d, i) => {
-    if (i % width === 0) process.stdout.write('\n')
-    if (d === missing) process.stdout.write(' ')
-    else process.stdout.write(chalk.bgHex(interpolateViridis(scale(d)))(' '))
-  })
-  process.stdout.write('\n')
-
-  // console.log(chalk.white.bgCyan(` time (${variables.time.attributes.units.value}): ${time} `))
-  // console.log(chalk.white.bgMagenta(` resolution: 1/${stride} `))
-}
-
-function animate (file) {
-  const dimensions = file.root.dimensions
-  const stride = Math.ceil(Math.max(
-    dimensions.lon.length / process.stdout.columns, dimensions.lat.length / (process.stdout.rows - 2)
-  ))
-  const width = Math.floor(dimensions.lon.length / stride)
-  const height = Math.floor(dimensions.lat.length / stride)
-
-  for (var i = 0; i < dimensions.time.length; i++) {
-    preview(file, i)
-    readline.moveCursor(process.stdout, -width, -height - 4)
-  }
+  // if (options.verbose) {
+  //   logH1('root')
+  //   logProps(file.root)
+  // }
+  // // attributes
+  // logH1('attributes')
+  // logAttributes(rootAttributes)
+  // // dimensions
+  // logH1('dimensions')
+  // logDimensions(rootDimensions, true)
+  // // variables
+  // logH1('variables')
+  // Object.keys(rootVariables).forEach(key => {
+  //   const {
+  //     name, type, attributes, dimensions
+  //   } = rootVariables[key]
+  //   // variable
+  //   if (options.verbose) {
+  //     logH2(name)
+  //     logProps(rootVariables[key])
+  //   } else {
+  //     logH2(`${name} (${type})`)
+  //   }
+  //   // dimensions
+  //   logH3('dimensions')
+  //   logDimensions(dimensions)
+  //   // attributes
+  //   logH3('attributes')
+  //   logAttributes(attributes)
+  // })
 }
 
 function convert (file) {
@@ -140,25 +75,19 @@ function convert (file) {
   const variables = file.root.variables
   const output = Object.keys(variables).map(key => {
     const variable = variables[key]
-    const {dimensions, name, attributes} = variable
 
-    dimensions[0].length = 1
-    const accessor = (dimensions.map((dimension, i) => [0, i === 0 ? 1 : dimension.length]).reduce((a, b) => a.concat(b), []))
-    console.log(accessor)
-    const data = variable.readSlice(...accessor)
-    // const data = extract(variable, 0, dimensions.map(d => 0))
-    console.log(data.length)
-    return {
-      variable: name,
-      attributes,
-      dimensions,
-      data
+    let values = []
+    if (key === 'time') {
+      values = variable.readSlice(0, 94).map(((d, i) => 2006 + i))
+    } else {
+      values = variable.readSlice(0, 94)
     }
+    return `${key},${values.join(',')}`
   })
 
   console.log('...')
   // console.log(output)
-  fs.writeFile(options.outputPath, JSON.stringify(output), err => {
+  fs.writeFile(options.outputPath, output.join('\n'), err => {
     if (err) throw err
     console.log(chalk.bold.white.bgGreen(`Saved to ${options.outputPath}`))
   })
